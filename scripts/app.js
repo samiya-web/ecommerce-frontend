2d941e135979b39f3b26eca8e9dabb0d8f1ab20d
Samiya Fatima <samiya648@gmail.com>
2025-12-29 14:01:01 +0530

    Update header, responsive navbar and hero section

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,8 @@
-console.log("E-Commerce Website Loaded");
+// Select elements
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector(".nav");
+
+// Toggle menu on hamburger click
+hamburger.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
