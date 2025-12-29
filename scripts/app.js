942f172fe55d4959e235be7d074b43fee8fa5934
Samiya Fatima <samiya648@gmail.com>
2025-12-29 18:54:38 +0530

    Add dynamic product grid using API

--- scripts/app.js
+++ scripts/app.js
@@ -7,3 +7,26 @@ hamburger.addEventListener("click", () => {
     nav.classList.toggle("active");
 
 });
+// ===== PRODUCT FETCH =====
+const productGrid = document.getElementById("productGrid");
+
+fetch("https://fakestoreapi.com/products")
+    .then(res => res.json())
+    .then(data => {
+        data.forEach(product => {
+            const card = document.createElement("div");
+            card.classList.add("product-card");
+
+            card.innerHTML = `
+                <img src="${product.image}" alt="${product.title}">
+                <h3>${product.title.substring(0, 40)}...</h3>
+                <p>₹ ${Math.round(product.price * 80)}</p>
+                <button>Add to Cart</button>
+            `;
+
+            productGrid.appendChild(card);
+        });
+    })
+    .catch(error => {
+        console.log("Error fetching products:", error);
+    });
